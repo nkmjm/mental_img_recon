dbadf630aeb87b8ab84e0811aacc68ca2a652875
K0118S <36918023+kencan7749@users.noreply.github.com>
2025-11-14 16:17:39 +0900

    Change import statement for recon_func module

--- recon_utils.py
+++ recon_utils.py
@@ -6,7 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 from IPython.display import clear_output, display
-import recon_func
+from . import recon_func
 
 
 def convert_featname(names, cvt_to='directory'):
